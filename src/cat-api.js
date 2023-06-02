e03c0fe146001ef55c3de3b6ae2909838d8a9af8
StanislavBatov <125702005+StanislavBatov@users.noreply.github.com>
2023-06-03 00:26:09 +0300

    4

---
+++ src/cat-api.js
@@ -0,0 +1,54 @@
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
+  'live_b13nP3upOqXnnzQ19OdqPyYeBxfaCbgHlfQAQupfcZee9oRqmaFvvYr8TERAkmdg';
+
+export function fetchBreeds() {
+  const url = `${BASE_URL}/breeds`;
+  const headers = {
+    'x-api-key': API_KEY,
+  };
+
+  return fetch(url, { headers })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (!data || data.length === 0) {
+        throw new Error('No data received from the server.');
+      }
+      return data;
+    })
+    .catch(error => {
+      console.log(error);
+      throw error;
+    });
+}
+
+
+export function fetchCatByBreed(breedId) {
+  const url = `${BASE_URL}/images/search?breed_ids=${breedId}`;
+  const headers = {
+    'x-api-key': API_KEY,
+  };
+
+  return fetch(url, { headers })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error fetching cat data.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (!data || data.length === 0) {
+        throw new Error('No cat data received from the server.');
+      }
+      return data[0].breeds[0];
+    })
+    .catch(error => {
+      console.log(error);
+      throw error;
+    });
+}
---
+++ src/index.js
@@ -0,0 +1,97 @@
+import SlimSelect from 'slim-select';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+
+const breedSelect = document.getElementById('breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
+
+let currentBreedId = null;
+let catData = null;
+
+function showLoader() {
+  loader.classList.remove('invisible');
+}
+
+function hideLoader() {
+  loader.classList.add('invisible');
+}
+
+function showError() {
+  error.classList.remove('invisible');
+}
+
+function hideError() {
+  error.classList.add('invisible');
+}
+
+function showCatInfo() {
+  catInfo.style.display = 'block';
+}
+
+function hideCatInfo() {
+  catInfo.style.display = 'none';
+}
+
+function displayCatInfo(catData) {
+  
+  const catName = catData.name;
+  const catDescription = catData.description;
+  const catTemperament = catData.temperament;
+  const catImageURL = catData.url;
+
+  const catInfoHTML = `
+    <img src="${catImageURL}" class="cat-img">
+    <h1>${catName}</h1>
+    <p>${catDescription}</p>
+    <p><span class="temperament">Temperament: </span>${catTemperament}</p>
+  `;
+
+  catInfo.innerHTML = catInfoHTML;
+}
+
+function handleBreedSelectChange(event) {
+  const selectedBreedId = event.target.value;
+  if (selectedBreedId !== currentBreedId) {
+    currentBreedId = selectedBreedId;
+    hideError();
+    hideCatInfo();
+    showLoader();
+
+    fetchCatByBreed(selectedBreedId)
+      .then(data => {
+        catData = data;
+        hideLoader();
+        showCatInfo();
+        displayCatInfo(catData);
+      })
+      .catch(error => {
+        hideLoader();
+        showError();
+      });
+  }
+}
+
+function initializeApp() {
+  showLoader();
+
+  fetchBreeds()
+    .then(breeds => {
+      hideLoader();
+      hideError();
+      breedSelect.disabled = false;
+      new SlimSelect({
+        select: breedSelect,
+        placeholder: 'Select a breed',
+        allowDeselect: true,
+        onChange: handleBreedSelectChange,
+        data: breeds.map(breed => ({ text: breed.name, value: breed.id })),
+      });
+    })
+    .catch(error => {
+      hideLoader();
+      showError();
+    });
+}
+
+initializeApp();
